599f326408428de89645385609a1d72587589996
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-13 19:41:17 -0300

    Program finished

--- LogsSite/Program.cs
+++ LogsSite/Program.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿using LogsSite.Entities;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
 namespace LogsSite {
     internal class Program {
         static void Main(string[] args) {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
             string path = "E:\\DEV-Files\\C#\\";
             string file = "Log.txt";
 
             try {
                 using (StreamReader sr = new StreamReader($"{path}{file}")) {
                     while (!sr.EndOfStream) {
-                        string line = sr.ReadLine();
-                        Console.WriteLine(line);
+                        string[] vect = sr.ReadLine().Split(' ');
+                        set.Add(new LogRecord(vect[0], DateTime.Parse(vect[1])));
                     }
+                    Console.WriteLine($"Total de usuários: {set.Count}");
                 }
             } catch (IOException e) {
                 Console.WriteLine($"Erro: {e.Message}");
