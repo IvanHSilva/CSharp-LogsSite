428d2db974e4e74eb52242491d98f90814332c25
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-13 19:22:32 -0300

    Read test file

--- LogsSite/Program.cs
+++ LogsSite/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace LogsSite {
     internal class Program {
         static void Main(string[] args) {
+            string path = "E:\\DEV-Files\\C#\\";
+            string file = "Log.txt";
+
+            try {
+                using (StreamReader sr = new StreamReader($"{path}{file}")) {
+                    while (!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            } catch (IOException e) {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+            Console.WriteLine();
         }
     }
 }
