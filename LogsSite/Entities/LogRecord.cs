d6487d489373430631fb2557456cc9056db209ed
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-13 19:27:31 -0300

    LogRecord class cretead

---
+++ LogsSite/Entities/LogRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogsSite.Entities {
+    public class LogRecord {
+        // Attributes
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        // Constructor
+        public LogRecord(string username, DateTime instant) {
+        Username = username;
+            Instant = instant;
+        }
+
+        // Overrides Methods
+        public override int GetHashCode() {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is LogRecord)) return false;
+            LogRecord other = (LogRecord)obj;
+            return Username.Equals(other.Username);
+        }
+
+    }
+}
